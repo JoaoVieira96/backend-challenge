f240272666b95dc6117f745686002986b88985ee
JoaoVieira <joaomgv96@gmail.com>
2023-07-01 16:43:07 +0100

    First approach to APIs and its service

---
+++ src/main/java/com/challenge/challenge/web/rest/StatisticResource.java
@@ -0,0 +1,36 @@
+package com.challenge.challenge.web.rest;
+
+import com.challenge.challenge.domain.responses.SumZoneTrips;
+import com.challenge.challenge.domain.responses.TopZones;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.Instant;
+
+public interface StatisticResource {
+
+    @RequestMapping(
+            path = {"/top-zones"},
+            produces = {"application/json"},
+            method = {RequestMethod.GET}
+    )
+    ResponseEntity<TopZones> topZones(@RequestParam(name = "order") String order);
+
+    @RequestMapping(
+            path = {"/zone-trips"},
+            produces = {"application/json"},
+            method = {RequestMethod.GET}
+    )
+    ResponseEntity<SumZoneTrips> zoneTrips(@RequestParam(name = "zone") Long zone, @RequestParam(name = "date") Instant date);
+
+
+    // ToDo
+    @RequestMapping(
+            path = {"/list-yellow"},
+            produces = {"application/json"},
+            method = {RequestMethod.GET}
+    )
+    ResponseEntity<Object> listYellow();
+}
---
+++ src/main/java/com/challenge/challenge/web/rest/impl/StatisticResourceImpl.java
@@ -0,0 +1,49 @@
+package com.challenge.challenge.web.rest.impl;
+
+import com.challenge.challenge.domain.exceptions.HttpException;
+import com.challenge.challenge.domain.exceptions.error.HttpError;
+import com.challenge.challenge.domain.responses.SumZoneTrips;
+import com.challenge.challenge.domain.responses.TopZones;
+import com.challenge.challenge.service.StatisticService;
+import com.challenge.challenge.web.rest.StatisticResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@RequestMapping("/api/v1")
+public class StatisticResourceImpl implements StatisticResource {
+
+    private final Logger log = LoggerFactory.getLogger(StatisticResourceImpl.class);
+    @Autowired
+    private StatisticService statisticService;
+
+    @Override
+    public ResponseEntity<TopZones> topZones(String order) {
+        log.info("topZones | REST request to list the first 5 top zones based on [{}]", order);
+
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<SumZoneTrips> zoneTrips(Long zone, Instant date) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> listYellow() {
+        return null;
+    }
+
+    @ExceptionHandler(HttpException.class)
+    public ResponseEntity<HttpError> handleAuthentication(HttpException e) {
+        return ResponseEntity.status(e.getStatus()).body(new HttpError(e.getStatus(), e.getMessage(), e.getDescription()));
+    }
+
+}
